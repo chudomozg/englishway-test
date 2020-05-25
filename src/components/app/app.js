9898aab8efa220b44254923e7299f383182c146b
chudomozg <chudomozg@gmail.com>
2020-05-25 14:49:36 +1000

    Initial commit

---
+++ src/components/app/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import QuestionList from "../question-list";
+import Total from "../total";
+
+function App() {
+  return (
+    <div className="App">
+      <QuestionList />
+      <Total />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/question-list/question-list.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class QuestionList extends Component() {
+  render() {
+    return <div>quest-list</div>;
+  }
+}
